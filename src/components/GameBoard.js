0235aea2a2d3de47d1f5bc31b04c7ba06b605d22
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-09-24 20:43:51 +0200

    Styled the X and O's

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -10,14 +10,17 @@ import '../css/GameBoard.css';
 // Creating the GameBoard component
 const GameBoard = ({ turn, setTurn, singlePlayer }) => {
 
+    const [gameover, setGameover] = useState(false);
+    const winningFields = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+    // Funciton that will handle the input for multiplayer
     const multiplayerGame = (e) => {
-        const field = e.target
-        field.innerHTML = turn ? "X" : "O"
         setTurn(!turn);
     }
 
+    // Function that will handle input for singleplayer
     const singlePlayerGame = (e) => {
-        console.log(e.target)
+        console.log(e.target.value)
     }
 
     // Function that will handle the input of the 
@@ -27,23 +30,23 @@ const GameBoard = ({ turn, setTurn, singlePlayer }) => {
     }
 
     return (
-        <div className='game-board'>
+        <div className={`game-board ${turn ? 'x' : 'circle'}`}>
             <Score turn={turn} />
             <div className="game">
                 <div className="row">
-                    <div className="field" onClick={handleClick}></div>
-                    <div className="field" onClick={handleClick}></div>
-                    <div className="field" onClick={handleClick}></div>
+                    <div className="field" data-value onClick={handleClick}></div>
+                    <div className="field" data-value onClick={handleClick}></div>
+                    <div className="field" data-value onClick={handleClick}></div>
                 </div>
                 <div className="row">
-                    <div className="field" onClick={handleClick}></div>
-                    <div className="field" onClick={handleClick}></div>
-                    <div className="field" onClick={handleClick}></div>
+                    <div className="field" data-value onClick={handleClick}></div>
+                    <div className="field" data-value onClick={handleClick}></div>
+                    <div className="field" data-value onClick={handleClick}></div>
                 </div>
                 <div className="row">
-                    <div className="field" onClick={handleClick}></div>
-                    <div className="field" onClick={handleClick}></div>
-                    <div className="field" onClick={handleClick}></div>
+                    <div className="field" data-value onClick={handleClick}></div>
+                    <div className="field" data-value onClick={handleClick}></div>
+                    <div className="field" data-value onClick={handleClick}></div>
                 </div>
             </div>
         </div>
